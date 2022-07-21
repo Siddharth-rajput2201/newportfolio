8a3279121c68f87f11787bce305348cc4b3167f6
Siddharth-rajput2201 <rajputsiddharth18@gmail.com>
2022-07-21 21:14:03 +0530

    Added Github Icon

--- lib/Utils/social_handle_widgets.dart
+++ lib/Utils/social_handle_widgets.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SocialHandleWidgets extends StatefulWidget {
   const SocialHandleWidgets({Key? key}) : super(key: key);
@@ -10,6 +13,32 @@ class SocialHandleWidgets extends StatefulWidget {
 class _SocialHandleWidgetsState extends State<SocialHandleWidgets> {
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.height;
+    return Row(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(25),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            child: Container(
+              height: height * 0.1,
+              width: width * 0.1,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(25),
+                border: Border.all(width: 2, color: Colors.white30),
+              ),
+              child: SvgPicture.asset(
+                "assets/images/githubsvg.svg",
+                color: Colors.white,
+                height: height * 0.075,
+                width: width * 0.075,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
