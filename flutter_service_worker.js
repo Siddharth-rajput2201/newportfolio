0a711e5c94b2c7317fb312cbb3b9ff26841d1a4a
Siddharth-rajput2201 <rajputsiddharth18@gmail.com>
2022-07-22 14:47:47 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,9 +25,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4fe4cb7c3176c5e22e6eca8bcaf1ebc7",
-"/": "4fe4cb7c3176c5e22e6eca8bcaf1ebc7",
-"main.dart.js": "3ffffc0c1b704de1e69c5c142dbbf875",
+"index.html": "dfd9aa994ef034832286adc5f162d3ec",
+"/": "dfd9aa994ef034832286adc5f162d3ec",
+"main.dart.js": "5641285fed0f23651d0af10c6ddf4ea8",
 "manifest.json": "7a7d7ae49b15046492f541ba57a04347",
 "version.json": "6a0e69daf7522fba270cce1a00bdbded"
 };
